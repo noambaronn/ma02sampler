c6892117c19a50666fdd3faf51fd50386a419ef7
noam <noam88888@gmail.com>
2022-01-16 22:00:29 +0200

    write to any kind of file output

--- src/main/java/workspace/sampler/Load/WriteToOutput.java
+++ src/main/java/workspace/sampler/Load/WriteToOutput.java
@@ -1,4 +1,10 @@
 package workspace.sampler.Load;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public interface WriteToOutput {
+    public void writeAsJson(List<Map<?, ?>> data, int numberOfLines) throws IOException;
 }
