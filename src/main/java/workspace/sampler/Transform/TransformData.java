712e4051cd4fcb09245dfb58299f170e86316bc6
noam <noam88888@gmail.com>
2022-01-16 21:59:41 +0200

    transform data

--- src/main/java/workspace/sampler/Transform/TransformData.java
+++ src/main/java/workspace/sampler/Transform/TransformData.java
@@ -1,4 +1,9 @@
 package workspace.sampler.Transform;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
 public interface TransformData {
+    public List<Map<?,?>> MakeChangesToData(List<Map<?,?>> data) throws IOException;
 }
